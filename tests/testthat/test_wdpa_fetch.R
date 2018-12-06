12eb0eb27c5443713ed294cfca878d18f4d8aefd
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-06 13:20:27 +1000

    add fetch tests, bar global

--- tests/testthat/test_wdpa_fetch.R
+++ tests/testthat/test_wdpa_fetch.R
@@ -16,11 +16,11 @@ test_that("ISO3", {
   expect_true(all(x$ISO3 == "LIE"))
 })
 
-test_that("global", {
-  skip_on_cran()
-  skip_if_not(pingr::is_online())
-  skip_if(mean(pingr::ping("www.google.com", count = 10)) > 10,
-          "slow internet connection detected")
-  x <- suppressWarnings(wdpa_fetch("global"))
-  expect_is(x, "sf")
-})
+# test_that("global", {
+#   skip_on_cran()
+#   skip_if_not(pingr::is_online())
+#   skip_if(mean(pingr::ping("www.google.com", count = 10)) > 10,
+#           "slow internet connection detected")
+#   x <- suppressWarnings(wdpa_fetch("global"))
+#   expect_is(x, "sf")
+# })
